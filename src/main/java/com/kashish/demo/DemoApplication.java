9102f8b2ab8ba912861aa23296fb70439fad1ea9
Kashish Pandey <kashishpandey494@gmail.com>
2026-07-13 12:53:19 +0530

    Next Changes made

---
+++ src/main/java/com/kashish/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.kashish.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		Student student= context.getBean(Student.class);
+		student.setName("Ram");
+		student.setAge(30);
+
+//		Student student = new Student(23, "Khushi");
+
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+	}
+}
