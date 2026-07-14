a2fd9c860bebc2ede7c67818dc774f29207a2529
Kashish Pandey <kashishpandey494@gmail.com>
2026-07-14 12:41:13 +0530

    Postman test done

---
+++ src/main/java/com/kashish/demo/StudentServer/StudentServer.java
@@ -0,0 +1,25 @@
+package com.kashish.demo.StudentServer;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServer {
+    //Store the student method;
+    @PostMapping("/create")
+    public String storeStudent(){
+        return "id: 1\n" +
+                "Name: Kashish\n" +
+                "Department: CSE\n" +
+                "Age: 22\n" ;
+
+    }
+    //Read the student with id
+
+    //update the student information
+
+    //delete the student information
+
+}
